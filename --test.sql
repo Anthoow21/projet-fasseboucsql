a09484e193adcec571a5ea6901089c42d4ac82d6
MathiasCros <crosm@3il.fr>
2024-12-10 09:49:05 +0100

    Update test.txt

---
+++ --test.sql
@@ -0,0 +1,7 @@
+--Comentaire pour le protype
+/* ChercherMembre
+-- Permet de chercher un utilisateur : 
+-- SELECT LoginUtilisateur FROM utilisateur WHERE LoginUtilisateur LIKE 
+SELECT CONCAT (préfixeLoginMembre,'%')
+-- th
+*/
